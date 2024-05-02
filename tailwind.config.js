00986578778a75461dd2da008d6624cfc5eda54a
thienbon <19127277@student.hcmus.edu.vn>
2024-05-02 21:45:00 +0700

    init project

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        'dark-green': '#4e6c50',
+        'green-grey': '#A2A378',
+        'brown': '#c1ae93',
+        'beige': '#a7ab90',
+        'red': '#FF0000	'
+      },
+      fontFamily: {
+        nunito:"Nunito"
+      },
+      fontSize:{
+        sm: "14px", 
+        md: "18px", 
+        lg: "24px", 
+        xl: "32px", 
+        base: "16px"
+
+      },
+    },
+  },
+  plugins: [],
+}
+
