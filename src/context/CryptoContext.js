0fd21c7ad0b90bc257af6a6de7ad61707ada9f18
thienbon <19127277@student.hcmus.edu.vn>
2024-05-20 22:59:08 +0700

    init project

--- src/context/CryptoContext.js
+++ src/context/CryptoContext.js
@@ -1,13 +1,29 @@
-import { createContext, useState } from "react";
+import { createContext, useState ,useLayoutEffect} from "react";
+import { json } from "react-router-dom";
 
 export const CryptoContext = createContext({});
 
 export const CryptoProvider = ({children}) => {
-    const [test,setTest] = useState("this is state");
+const [CryptoData, setCryptoData] = useState()
+const getCryptoData = async () => {
+    try {
+        const data = await fetch('https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d'
 
+        ).then(res => res.json()).then(json => json);
+        console.log(data);
+        setCryptoData(data);
+        
+    } catch (error) {
+        console.log(error);
+    }
+};
+useLayoutEffect(() => {
+    getCryptoData();
+  
+}, [])
 
     return(
-        <CryptoContext.Provider value={{ test }}>
+        <CryptoContext.Provider value={{CryptoData}}>
         {children}    
 
 
